d61232c2bc127e3fdc55f39595cbb46ccfc2ab82
Daniel Reinhuber <dreinhuber@gmail.com>
2022-03-18 20:57:04 -0500

    repo initialized with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: path.join(__dirname, "/client/src", "index.js"),
+    output: { path: path.join(__dirname, "/client/build"), filename: "index.bundle.js" },
+    mode: process.env.NODE_ENV || "development",
+    resolve: { modules: [path.resolve(__dirname, "/client/src"), "node_modules"] },
+    devServer: { static: path.join(__dirname, "/client/src") },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"]
+        }
+      ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "/client/src", "index.html"),
+        }),
+    ],
+};
